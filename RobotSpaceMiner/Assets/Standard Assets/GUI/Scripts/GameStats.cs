c408b5a1d81376a8fac14aedca639aacf21db932
bbricker87 <bbricker09@gmail.com>
2014-08-20 10:47:55 -0400

    Fixed conflicts

--- RobotSpaceMiner/Assets/Standard Assets/GUI/Scripts/GameStats.cs
+++ RobotSpaceMiner/Assets/Standard Assets/GUI/Scripts/GameStats.cs
@@ -4,19 +4,35 @@ using System.Collections;
 public class GameStats : MonoBehaviour {
 	
 	private int score;
-
+	private int distance;
+	private float time;
 	public int Score {
-		get { return score;}
+		get { return score; }
+	}
+	public int Distance {
+		get { return distance; }
 	}
 
+	public void SetDistance(int value)
+	{
+		if (StateManager.Instance.CurrentState == StateManager.State.PLAYING)
+			this.distance = value;
+	}
 	public void AddScore(int value)
 	{
-		this.score += value;
+		if (StateManager.Instance.CurrentState == StateManager.State.PLAYING)
+			this.score += value;
+	}
+	public void SetTime(float value)
+	{
+		this.time = value;
 	}
 	
 	public void LogStats()
 	{
-        DataLogger.Instance.Log("Total Score: " + Score);
+		DataLogger.Instance.Log("Time: " + time +
+		                        " Score: " + Score +
+		                        " Distance: " + Distance);
 	}
 
 }
