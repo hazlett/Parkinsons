d5106c667f9aad42d530de0e70654ed83b7c9942
hazlett <hazlett.206@gmail.com>
2014-08-21 17:47:48 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	RobotSpaceMiner/RobotSpaceMiner-csharp.v11.suo

---
+++ RobotSpaceMiner/Assets/Standard Assets/Scripts/RunMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using OhioState.Libraries.Gesture;
+using OhioState.Kinect;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+
+public class RunMovement : IGestureAction
+{
+
+    private BasicMovement cart;
+
+    public RunMovement(BasicMovement cart)
+    {
+        this.cart = cart;
+    }
+
+    public void Trigger(object data)
+    {
+        cart.Move();
+    }
+}
