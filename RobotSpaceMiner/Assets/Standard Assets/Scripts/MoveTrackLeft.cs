c88ffa5fd8fdee5c0f712b8f9be0cdd019605f90
David Hazlett <hazlett.206@gmail.com>
2014-08-22 13:27:32 -0400

    Hop Track Template

---
+++ RobotSpaceMiner/Assets/Standard Assets/Scripts/MoveTrackLeft.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using OhioState.Libraries.Gesture;
+
+public class MoveTrackLeft : IGestureAction {
+
+	private BasicMovement cart;
+	public MoveTrackLeft(BasicMovement cart)
+	{
+		this.cart = cart;
+	}
+	public void Trigger(object data)
+	{
+
+	}
+}
---
+++ RobotSpaceMiner/Assets/Standard Assets/Scripts/MoveTrackRight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using OhioState.Libraries.Gesture;
+
+public class MoveTrackRight : IGestureAction {
+
+	private BasicMovement cart;
+	public MoveTrackRight(BasicMovement cart)
+	{
+		this.cart = cart;
+	}
+	public void Trigger(object data)
+	{
+
+	}
+}
