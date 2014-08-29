b94afffbfa88b58832e5b0697e2902474cd99da8
bbricker87 <bbricker09@gmail.com>
2014-08-29 10:42:14 -0400

    Cart Handbar Rotates

---
+++ RobotSpaceMiner/Assets/Standard Assets/Scripts/HandbarRotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandbarRotation : MonoBehaviour {
+
+    public Rigidbody cart;
+    private float scale = 2.0f;
+    private bool switchDirections = false;
+    private Vector3 direction = Vector3.right;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "cart")
+        {
+            SwitchDirections();
+        }
+    }
+    void Update()
+    {
+        gameObject.transform.Rotate(direction * Time.deltaTime * cart.velocity.x * scale, Space.Self);
+    }
+
+    void SwitchDirections()
+    {
+        if (switchDirections)
+        {
+            switchDirections = false;
+            direction = Vector3.right;
+        }
+        else
+        {
+            switchDirections = true;
+            direction = Vector3.left;
+        }
+    }
+}
---
+++ RobotSpaceMiner/Assets/Standard Assets/Scripts/ParticleRandomizer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRandomizer : MonoBehaviour {
+
+    private ParticleSystem popUp;
+    private bool newLocation = false;
+    private float newX, newY, newZ, startX, startY, startZ;
+    private int moveCount;
+
+	// Use this for initialization
+	void Start () {
+
+        popUp = this.particleSystem;
+
+        moveCount = 0;
+
+        startX = this.popUp.transform.localPosition.x;
+        startY = this.popUp.transform.localPosition.y;
+        startZ = this.popUp.transform.localPosition.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (popUp.isPlaying)
+        {
+            newLocation = false;
+            moveCount = 0;
+        }
+        if (popUp.isStopped)
+        {
+            newLocation = true;
+        }
+
+        MovePopup();
+	}
+
+    void MovePopup()
+    {
+        newX = Random.Range(-0.5f, 0.5f);
+        newY = Random.Range(-0.5f, 0.5f);
+        newZ = Random.Range(-0.5f, 0.5f);
+
+        if (newLocation && moveCount == 0)
+        {
+            this.popUp.transform.localPosition = new Vector3(startX + newX, startY + newY, startZ + newZ);
+            moveCount++;
+        }
+    }
+}
